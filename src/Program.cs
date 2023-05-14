7e9e2e427de7953ef935105cb0b805e15d4b3a52
laurentfaggin <laurentfaggin@hotmail.fr>
2023-05-14 08:02:56 -0400

    finalisation

---
+++ src/Program.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace revision_poo1
+{
+    internal class Program
+    {
+        private static void main(string[] args)
+        {
+            Liquide cafe = new Cafe(25);
+            Liquide creme = new Creme(10);
+            Liquide lait = new Lait(20);
+            Liquide chocolat = new Chocolat(50);
+
+            Contenant bol = new Bol();
+            Contenant tasse = new Tasse();
+
+            bol.AjouterLiquide(cafe);
+            bol.AjouterLiquide(creme);
+            bol.AjouterLiquide(lait);
+            tasse.AjouterLiquide(chocolat);
+
+            Console.WriteLine(bol.ToString());
+        }
+    }
+}
